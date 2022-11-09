655c26ad559b8505a0308bc9aea54224016f56ef
uncoolzero <107518216+uncoolzero@users.noreply.github.com>
2022-11-09 17:21:44 -0300

    Mobile: Search bar enabled
    Tapping the search button now brings up the search bar

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,16 +3,53 @@ import { HiMenu } from "react-icons/hi"
 import { CgProfile } from 'react-icons/cg'
 import { IoWalletOutline } from 'react-icons/io5'
 
+function searchButtonClick() {
+
+var searchbar = document.getElementById("searchbar")
+var logo = document.getElementById("logo")
+var menu = document.getElementById("hamburgermenu")
+var input = document.getElementById("searchbox")
+
+if (searchbar)
+{
+  if (searchbar.className == "search-closed")
+  {
+    searchbar.className = "search-open"
+    //@ts-ignore
+    logo.className = "logo-search-open"
+    //@ts-ignore
+    menu.className = "menu-search-open"
+    //@ts-ignore
+    input.className = "input-search-open"
+  }
+  else
+  {
+    searchbar.className = "search-closed"
+    //@ts-ignore
+    logo.className = "logo-search-closed"
+    //@ts-ignore
+    menu.className = "menu-search-closed"
+    //@ts-ignore
+    input.className = "input-search-closed"
+  }
+}
+
+}
 
 function Header() {
 
 return (
 
   <header className="bg-[#0d1525]/90 backdrop-blur-xl sticky top-0 z-20 w-full px-6 py-4 font-bold text-lg gap-x-3 flex flex-row md:py-6 xl:py-4">
-    <div className="basis-full place-self-center md:text-xl md:basis-1">NFTSITE</div>
-    <div className="text-2xl transition-all ease-in-out duration-150 hover:bg-slate-600 focus-within:bg-slate-700 place-self-center md:place-items-center md:flex md:text-3xl xl:text-xl md:text-neutral-400 md:basis-full md:mx-4 md:bg-slate-800 md:rounded-md md:pl-3 md:py-3 xl:py-1">
+    <div id="logo" className="logo-search-closed">NFTSITE</div>
+    <div id="searchbar" className="search-closed">
+      <div onClick={searchButtonClick} className="md:hidden">
       <FaSearch />
-      <input type="text" placeholder='Search items and collections' className="hidden focus:outline-none placeholder:bg-transparent bg-transparent md:flex text-neutral-100 font-normal text-2xl pl-2 xl:text-xl place-self-center w-full"></input>
+      </div>
+      <div className="hidden md:flex">
+      <FaSearch />
+      </div>
+      <input id="searchbox" type="text" placeholder='Search items and collections' className="input-search-closed"></input>
     </div>
     <div className="hidden place-items-center lg:flex gap-x-6">
         <button className="hidden xl:inline-block hover:bg-slate-600 px-2 py-1 rounded-md transition-all ease-in-out">Explore</button>
@@ -22,7 +59,7 @@ return (
         <button className="text-4xl xl:text-3xl hover:text-slate-300 rounded-md transition-all ease-in-out"><CgProfile /></button>
         <button className="text-4xl xl:text-3xl hover:text-slate-300 rounded-md transition-all ease-in-out"><IoWalletOutline /></button>
       </div>
-    <div className="text-3xl place-self-center md:text-4xl xl:hidden lg:pl-4"><HiMenu /></div>
+    <div id="hamburgermenu" className="menu-search-closed"><HiMenu /></div>
   </header>
 
 )
