5c3d111238eccae087e852a89735b32bdeea0c20
uncoolzero <107518216+uncoolzero@users.noreply.github.com>
2022-11-09 15:38:47 -0300

    Mobile view fix
    Fixed misaligned button in mobile mode

--- components/Communities.tsx
+++ components/Communities.tsx
@@ -4,11 +4,11 @@ function Communities() {
 
 return(
     <div className="bg-slate-800 w-full text-3xl flex flex-col rounded-lg md:flex-row md:content-center">
-        <div className="w-full px-8 md:place-content-center rounded-lg md:flex md:flex-col md:px-0 bg-slate-800">
+        <div className="w-full px-8 md:mx-2 place-content-center rounded-lg md:flex md:flex-col md:px-0 bg-slate-800">
             <div className="place-self-center text-3xl  text-center mt-4">Create your own Community</div>
             <div className="place-self-center text-base text-center mt-4">
                 <a className="underline decoration-dotted underline-offset-4 cursor-pointer hover:text-blue-400 transition-all ease-in-out">Terms and Conditions</a> apply</div>
-            <button className="bg-blue-500 hover:bg-blue-400 transition-all ease-in-out rounded-lg place-self-center my-4 text-lg px-8 py-2 text-center">Create Community</button>
+            <button className="bg-blue-500 hover:bg-blue-400 transition-all ease-in-out rounded-lg w-full md:w-[90%] place-self-center my-4 text-lg px-8 py-2 text-center">Create Community</button>
         </div>
         <div className="hidden md:flex w-24 -mr-8 rounded-l-lg bg-gradient-to-r z-10 from-slate-800" />
         <div className="flex flex-row overflow-x-scroll scrollbar-hide mx-2 mb-6 pl-4 pr-4 snap-x snap-mandatory gap-x-8 md:gap-x-4 md:pt-6 md:w-[170%] lg:w-[250%]">
