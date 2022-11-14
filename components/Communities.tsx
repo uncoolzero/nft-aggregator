640ddd7ffeb4b312bd913ed043c1c4116c9a3692
uncoolzero <107518216+uncoolzero@users.noreply.github.com>
2022-11-14 09:47:08 -0300

    More tweaks
    Tweaked spacings on trending section
    Navigation arrows on the communities section

--- components/Communities.tsx
+++ components/Communities.tsx
@@ -1,4 +1,5 @@
 import data from "../data/data"
+import { MdNavigateNext, MdNavigateBefore } from 'react-icons/md'
 
 function Communities() {
 
@@ -11,26 +12,30 @@ return(
             <button className="bg-blue-500 hover:bg-blue-400 transition-all ease-in-out rounded-lg w-full md:w-[90%] place-self-center my-4 text-lg px-8 py-2 text-center text-white shadow-md">Create Community</button>
         </div>
         <div className="hidden md:flex w-24 -mr-8 rounded-l-lg bg-gradient-to-r z-10 dark:from-slate-800 from-slate-300" />
-        <div className="flex flex-row overflow-x-scroll scrollbar-hide mx-2 mb-6 pl-4 pr-4 snap-x snap-mandatory gap-x-8 md:gap-x-4 md:pt-6 md:w-[170%] lg:w-[250%]">
-        {data.map((data) =>
-            <div key={data.name} className={`min-w-full md:min-w-[47%] lg:min-w-[32%] bg-cover bg-center rounded-lg overflow-clip`} style={{backgroundColor: `rgb(${data.color})`}}>
-                    <div className={`px-4 py-4 snap-center rounded-lg`}>
-                        <img className="rounded-lg w-full object-cover" src={data.image} />
-                        <div className="text-lg text-white font-bold py-2">{data.name}</div>
-                        <div className="text-sm h-36 md:h-44 text-white/80">{data.description}</div>
-                        <div className="flex flex-row text-base gap-x-4">
-                            <div className="border-r border-white/20 pr-4">
-                                <div className="text-white/80">Volume</div>
-                                <div className="text-white font-bold">{data.volume} ETH</div>
-                            </div>
-                            <div>
-                                <div className="text-white/80">Floor</div>
-                                <div className="text-white font-bold">{data.floorPrice} ETH</div>
+        <div className="flex flex-row relative w-full md:w-[70%]">
+            <div className="trending-scroll-button left-2"><MdNavigateBefore /></div>    
+            <div className="flex flex-row overflow-x-scroll scrollbar-hide mx-2 mb-6 pl-4 pr-4 snap-x snap-mandatory gap-x-8 md:gap-x-4 md:pt-6 md:w-[170%] lg:w-[250%]">
+            {data.map((data) =>
+                <div key={data.name} className={`min-w-full md:min-w-[47%] lg:min-w-[32%] bg-cover bg-center rounded-lg overflow-clip`} style={{backgroundColor: `rgb(${data.color})`}}>
+                        <div className={`px-4 py-4 snap-center rounded-lg`}>
+                            <img className="rounded-lg w-full object-cover" src={data.image} />
+                            <div className="text-lg text-white font-bold py-2">{data.name}</div>
+                            <div className="text-sm h-36 md:h-44 text-white/80">{data.description}</div>
+                            <div className="flex flex-row text-base gap-x-4">
+                                <div className="border-r border-white/20 pr-4">
+                                    <div className="text-white/80">Volume</div>
+                                    <div className="text-white font-bold">{data.volume} ETH</div>
+                                </div>
+                                <div>
+                                    <div className="text-white/80">Floor</div>
+                                    <div className="text-white font-bold">{data.floorPrice} ETH</div>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                </div>
+            )}
             </div>
-        )}
+            <div className="trending-scroll-button right-2"><MdNavigateNext /></div>
         </div>
     </div>
 )
